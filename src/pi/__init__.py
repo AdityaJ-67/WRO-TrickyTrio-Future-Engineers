26c90426ef84da0ee080a2de1aaf981585b668e1
Aditya Juneja <epic.aj1470@gmail.com>
2026-08-21 22:21:59 +0530

    Delete src directory

--- src/pi/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Pi 5 'Brain': vision, mission state machine, planning, serial master.
-
-Nothing in this package may block for longer than a control period, and
-nothing in it is trusted by the Pico. If this whole process dies, the Pico
-stops the vehicle on its own 300 ms link timeout -- that is the contract.
-"""
